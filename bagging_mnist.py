a84adb356ba7b55717da6892cf19ae2f03666437
yuanFang1 <32661106+yuanFang1@users.noreply.github.com>
2018-01-25 14:21:52 +0800

    Update bagging_mnist.py

--- bagging_mnist.py
+++ bagging_mnist.py
@@ -11,7 +11,7 @@ mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 print(mnist.train.images.shape)
 index_in_epoch = 0
 epoch_completed =0
-num_of_weak = 5
+num_of_weak = 20
 num_of_traindata = mnist.train.images.shape[0]
 num_of_testdata = mnist.test.images.shape[0]
 train_data = np.zeros((num_of_weak,num_of_traindata,784))
@@ -19,7 +19,7 @@ train_label = np.zeros((num_of_weak,num_of_traindata,10))
 
 _train_data = np.zeros((num_of_weak,num_of_traindata,784))
 _train_label = np.zeros((num_of_weak,num_of_traindata,10))
-
+print(mnist.test.labels.shape)
 for i in range(num_of_weak):
     index = np.random.randint(0,num_of_traindata-1,(1,num_of_traindata))
     train_data[i] =mnist.train.images[index]
@@ -43,8 +43,8 @@ keep_prob = tf.placeholder(tf.float32) # dropout (keep probability)
 def next_batch():
     global index_in_epoch
     global  epoch_completed
-    print(index_in_epoch)
-    print(num_of_traindata)
+    # print(index_in_epoch)
+    # print(num_of_traindata)
     start = index_in_epoch
     if index_in_epoch ==0 and epoch_completed ==0 :
         for data_index in range(num_of_weak):
@@ -138,7 +138,7 @@ def create_graph():
 
     # Construct model
     logits = conv_net(X, weights, biases, keep_prob)
-    prediction = tf.nn.softmax(logits)
+    prediction = tf.argmax(tf.nn.softmax(logits),1)
 
     # Define loss and optimizer
     loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
@@ -148,14 +148,13 @@ def create_graph():
 
 
     # Evaluate model
-    correct_pred = tf.equal(tf.argmax(prediction, 1), tf.argmax(Y, 1))
+    correct_pred = tf.equal(prediction, tf.argmax(Y, 1))
     accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
     return weights,biases,loss_op,train_op,prediction,accuracy
-weights,biases,loss_op,train_op,prediction,accuracy =create_graph()
-weights2,biases2,loss_op2,train_op2,prediction2,accuracy2 =create_graph()
-weights3,biases3,loss_op3,train_op3,prediction3,accuracy3 =create_graph()
-weights4,biases4,loss_op4,train_op4,prediction4,accuracy4 =create_graph()
-weights5,biases5,loss_op5,train_op5,prediction5,accuracy5 =create_graph()
+# weights0,biases0,loss_op0,train_op0,prediction0,accuracy0 =create_graph()
+
+for i in range(num_of_weak):
+    exec ('weights{},biases{},loss_op{},train_op{},prediction{},accuracy{},=create_graph()'.format(i,i,i,i,i,i))
 
 # Initialize the variables (i.e. assign their default value)
 init = tf.global_variables_initializer()
@@ -171,125 +170,33 @@ with tf.Session() as sess:
     for step in range(1, num_steps+1):
         batch_x, batch_y = next_batch()
         # Run optimization op (backprop)
-        _,t_loss1,t_acc1 = sess.run([train_op,loss_op,accuracy], feed_dict={X: batch_x[0], Y: batch_y[0], keep_prob: 0.5})
-        _, t_loss2, t_acc2 = sess.run([train_op2, loss_op2, accuracy2], feed_dict={X: batch_x[1], Y: batch_y[1], keep_prob: 0.5})
-        _, t_loss3, t_acc3 = sess.run([train_op3, loss_op3, accuracy3], feed_dict={X: batch_x[2], Y: batch_y[2], keep_prob: 0.5})
-        _, t_loss4, t_acc4 = sess.run([train_op4, loss_op4, accuracy4], feed_dict={X: batch_x[3], Y: batch_y[3], keep_prob: 0.5})
-        _, t_loss5, t_acc5 = sess.run([train_op5, loss_op5, accuracy5], feed_dict={X: batch_x[4], Y: batch_y[4], keep_prob: 0.5})
-
-        if step % 1000 == 0 :
+        pred = np.zeros((num_of_weak,10000))
+        test_acc = np.zeros(num_of_weak)
+        test_loss = np.zeros(num_of_weak)
+        for j in range(num_of_weak):
+            t = str(j)
             # Calculate batch loss and accuracy
-            train_loss1, train_acc1 = sess.run([loss_op, accuracy], feed_dict={X: batch_x[0],
-                                                                 Y: batch_y[0],
-                                                                 keep_prob: 1.0})
-            train_loss2, train_acc2 = sess.run([loss_op2, accuracy2], feed_dict={X: batch_x[1],
-                                                                               Y: batch_y[1],
-                                                                               keep_prob: 1.0})
-            train_loss3, train_acc3 = sess.run([loss_op3, accuracy3], feed_dict={X: batch_x[2],
-                                                                               Y: batch_y[2],
-                                                                               keep_prob: 1.0})
-            train_loss4, train_acc4 = sess.run([loss_op4, accuracy4], feed_dict={X: batch_x[3],
-                                                                                 Y: batch_y[3],
-                                                                                 keep_prob: 1.0})
-            train_loss5, train_acc5 = sess.run([loss_op5, accuracy5], feed_dict={X: batch_x[4],
-                                                                                 Y: batch_y[4],
-                                                                                 keep_prob: 1.0})
+            exec('_,train_loss'+t+',train_acc'+t+' = sess.run([train_op'+t+',loss_op'+t+\
+        	',accuracy'+t+'],feed_dict={X:batch_x['+t+'],Y:batch_y['+t+'],keep_prob:0.5})')
 
-            print("Step " + str(step) + ", Minibatch Loss= " + \
-                  "{:.4f}".format(train_loss1) + ", Training Accuracy= " + \
-                  "{:.3f}".format(train_acc1))
-            print("Step " + str(step) + ", Minibatch Loss= " + \
-                  "{:.4f}".format(train_loss2) + ", Training Accuracy= " + \
-                  "{:.3f}".format(train_acc2))
-            print("Step " + str(step) + ", Minibatch Loss= " + \
-                  "{:.4f}".format(train_loss3) + ", Training Accuracy= " + \
-                  "{:.3f}".format(train_acc3))
-            print("Step " + str(step) + ", Minibatch Loss= " + \
-                  "{:.4f}".format(train_loss4) + ", Training Accuracy= " + \
-                  "{:.3f}".format(train_acc4))
-            print("Step " + str(step) + ", Minibatch Loss= " + \
-                  "{:.4f}".format(train_loss5) + ", Training Accuracy= " + \
-                  "{:.3f}".format(train_acc5))
-            acc =0
-            loss=0
-            test_acc=[0,0,0,0,0,0]
-            test_loss=[0,0,0,0,0]
-            for i in range(10):
-                pred=[]
-                start_index = i * 1000
-                end_index = (i + 1) * 1000
-                pred1,acc,loss= sess.run([prediction,accuracy,loss_op], feed_dict={X: mnist.test.images[start_index:end_index],
-                                                     Y: mnist.test.labels[start_index:end_index],
-                                                     keep_prob: 1.0})
-                test_loss[0]+=loss
-                test_acc[0]+=acc
-                pred2,acc, loss = sess.run([prediction2,accuracy2, loss_op2], feed_dict={X: mnist.test.images[start_index:end_index],
-                                                                     Y: mnist.test.labels[start_index:end_index],
-                                                                     keep_prob: 1.0})
-                test_loss[1] += loss
-                test_acc[1] += acc
+            if step % 1000 == 0:
+                # print batch loss and accuracy
+                exec("print(\"Step " + str(step) + ", Minibatch Loss= " + \
+                     "%.4f\"%train_loss" + t + "+\", Training Accuracy = %.4f\"%train_acc" + t + ")")
 
-                pred3,acc, loss = sess.run([prediction3,accuracy3, loss_op3], feed_dict={X: mnist.test.images[start_index:end_index],
-                                                                       Y: mnist.test.labels[start_index:end_index],
-                                                                       keep_prob: 1.0})
-                test_loss[2] += loss
-                test_acc[2] += acc
+                exec('pred[{}],test_acc[{}],test_loss[{}]= sess.run([prediction{},accuracy{},loss_op{}],'.format(j, j, j, j, j, j)+ \
+                        'feed_dict={X: mnist.test.images,Y: mnist.test.labels,keep_prob: 1.0})')
+                print("Step " + str(step) + ", Minibatch Loss= " + \
+                      "{:.6f}".format(test_loss[j]) + ", test Accuracy= " + \
+                      "{:.6f}".format(test_acc[j]) + "  time= " + "{:.6f}".format(time.clock() - start))
+        if(step%1000==0):
+            temp = np.zeros(10000)
+            for j in range(10000):
+                word_counts = Counter(pred[:, j])
+                temp[j] = word_counts.most_common(1)[0][0]
+            acc = np.mean(np.equal(temp, np.argmax(mnist.test.labels, 1)))
+            print("end_acc :%.6f" % acc)
 
-                pred4, acc, loss = sess.run([prediction4, accuracy4, loss_op4],
-                                            feed_dict={X: mnist.test.images[start_index:end_index],
-                                                       Y: mnist.test.labels[start_index:end_index],
-                                                       keep_prob: 1.0})
-                test_loss[3] += loss
-                test_acc[3] += acc
-
-                pred5, acc, loss = sess.run([prediction5, accuracy5, loss_op5],
-                                            feed_dict={X: mnist.test.images[start_index:end_index],
-                                                       Y: mnist.test.labels[start_index:end_index],
-                                                       keep_prob: 1.0})
-                test_loss[4] += loss
-                test_acc[4] += acc
-
-                pred1 = np.argmax(pred1,1)
-                pred2 = np.argmax(pred2,1)
-                pred3 = np.argmax(pred3, 1)
-                pred4 = np.argmax(pred4, 1)
-                pred5 = np.argmax(pred5, 1)
-
-                pred.append(pred1)
-                pred.append(pred2)
-                pred.append(pred3)
-                pred.append(pred4)
-                pred.append(pred5)
-
-                pred = np.array(pred)
-                temp = np.zeros((1000))
-                for j in range(1000):
-                    word_counts = Counter(pred[:,j])
-                    temp[j] = word_counts.most_common(1)[0][0]
-
-                acc = np.mean(np.equal(temp,np.argmax(mnist.test.labels[start_index:end_index],1)))
-                # acc ,temppred= sess.run([end_accuracy,end_pred], feed_dict={X: mnist.test.images[start_index:end_index],
-                #                                                        Y: mnist.test.labels[start_index:end_index],
-                #                                                        keep_prob: 1.0})
-                # print(temppred.shape)
-                test_acc[5] += acc
-
-            print("Step " + str(step) + ", Minibatch Loss= " + \
-                      "{:.6f}".format(test_loss[0] / 10) + ", test Accuracy= " + \
-                      "{:.6f}".format(test_acc[0] / 10) + "  time= " + "{:.6f}".format(time.clock() - start))
-            print("Step " + str(step) + ", Minibatch Loss= " + \
-                  "{:.6f}".format(test_loss[1] / 10) + ", test Accuracy= " + \
-                  "{:.6f}".format(test_acc[1] / 10) + "  time= " + "{:.6f}".format(time.clock() - start))
-            print("Step " + str(step) + ", Minibatch Loss= " + \
-                  "{:.6f}".format(test_loss[2] / 10) + ", test Accuracy= " + \
-                  "{:.6f}".format(test_acc[2] / 10) + "  time= " + "{:.6f}".format(time.clock() - start))
-            print("Step " + str(step) + ", Minibatch Loss= " + \
-                  "{:.6f}".format(test_loss[3] / 10) + ", test Accuracy= " + \
-                  "{:.6f}".format(test_acc[3] / 10) + "  time= " + "{:.6f}".format(time.clock() - start))
-            print("Step " + str(step) + ", Minibatch Loss= " + \
-                  "{:.6f}".format(test_loss[4] / 10) + ", test Accuracy= " + \
-                  "{:.6f}".format(test_acc[4] / 10) + "  time= " + "{:.6f}".format(time.clock() - start))
-            print("end_acc :%.6f"%(test_acc[5]/10))
     print("cost_time: %.6f"% (time.clock() - start))
     print("Optimization Finished!")
 
